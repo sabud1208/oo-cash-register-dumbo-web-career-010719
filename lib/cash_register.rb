53a5bd16ce1976adce40daff543588a0e0b3577e
Stephanie Abud <sabud1208@gmail.com>
2019-01-13 05:07:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,17 +20,23 @@ def add_item(title, price, quantity= 1)
   
   def apply_discount
     if discount != 0
-      self.total = (total * ((100 - discount.to_f)/100)).to_i
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
       "After the discount, the total comes to $#{self.total}."
     else
       "There is no discount to apply."
     end
   end
 
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
-  end
+def items
+    new_register = []
+    @items.each do |x|
+      new_register << x
+    end
+    new_register
 
+end
+  
+  
 
 end
 
